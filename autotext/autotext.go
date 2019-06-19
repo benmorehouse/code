d24beb4a2304c50ea140b18bff5a790fbc00710b
Benjamin Morehouse <bmorehouse@overlooked.com>
2019-06-19 16:01:03 -0700

    new stuff

---
+++ autotext/autotext.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"os/exec"
+)
+
+
+func main(){
+	//contact information is a struct with a name and phone number in each slice
+	contactInformation := infoReader()
+
+}
+
---
+++ garbage/demo.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	name := "ben"
+	number := "5136028241"
+}
+
+
